b4865b2066498eb819ef4a13643be6b65eceb7c4
NessieNaw <34487013+NessieNaw@users.noreply.github.com>
2019-03-13 14:29:48 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 1);
+            this.button1.Margin = new System.Windows.Forms.Padding(48, 55, 48, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Down";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 1);
+            this.button2.Margin = new System.Windows.Forms.Padding(48, 55, 48, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(553, 1);
+            this.button3.Margin = new System.Windows.Forms.Padding(48, 55, 48, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 53);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX.LabelStyle.Format = "\'10^\'0";
+            chartArea1.AxisX.Maximum = 2D;
+            chartArea1.AxisX.Minimum = -1D;
+            chartArea1.AxisX.Title = "L/d";
+            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.DarkRed;
+            chartArea1.AxisY.LabelStyle.Format = "\'10^\'0";
+            chartArea1.AxisY.Maximum = 5D;
+            chartArea1.AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Horizontal;
+            chartArea1.AxisY.Title = "Rt/Rm";
+            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.DarkRed;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ContextMenuStrip = this.contextMenuStrip1;
+            legend1.Name = "Rt/Rm";
+            legend1.Title = "Rt/Rm";
+            legend1.TitleForeColor = System.Drawing.Color.DarkRed;
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(21, 100);
+            this.chart1.Margin = new System.Windows.Forms.Padding(21, 36, 21, 36);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Rt/Rm";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(5190, 2816);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "c";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 34);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening_1);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(123, 30);
+            this.copyToolStripMenuItem.Text = "copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click_1);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(88, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(267, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(27, 64);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "D/d = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(197, 64);
+            this.label4.Margin = new System.Windows.Forms.Padding(0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ri/Rm = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 891);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(9, 17, 9, 17);
+            this.Name = "Form1";
+            this.Text = "Teoretyczne krzywe gradientowe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Diagnostics;
+
+namespace test2
+{
+    public partial class Form1 : Form
+    {
+        const string fileName = "C:/Users/Agnieszka/Desktop/BAZA2.bin";
+        List<float> valX = new List<float>();
+        List<float> valY = new List<float>();
+        int v = 0;
+
+        public void createChart(int step)
+        {
+            float readValue;
+            FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            using (BinaryReader reader = new BinaryReader(file))
+            {
+                chart1.Series.Clear();
+                for(int i = 0; i < 120; i++)
+                {                    
+                   //odczyt xów
+                     readValue = reader.ReadSingle();
+                     valX.Add(readValue);
+                 }
+                reader.BaseStream.Position = ((step * 123 * 38) + 120) * sizeof(Single);
+                float label;
+                for (int j = 0; j < 38; j++)
+                {
+                    valY.Clear();
+                    label1.Text = reader.ReadSingle().ToString();
+                    label2.Text = reader.ReadSingle().ToString();
+                    label = reader.ReadSingle();
+
+                    for (int i = 0; i < 120; i++)
+                    {
+                        readValue = reader.ReadSingle();
+                        valY.Add(readValue);
+                    }
+                    chart1.Series.Add("Line " + j.ToString());
+                    chart1.Series["Line " + j.ToString()].LegendText = label.ToString();
+                    chart1.Series["Line " + j.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    for (int i = 0; i < 120; i++)
+                    {
+                        chart1.Series["Line " + j.ToString()].Points.AddXY(valX[i], valY[i]);
+                    }
+                }
+
+            }
+            
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // v--;
+            if (v == 38)
+                v -= 38;
+            else
+                v--;
+            if (v == 0)
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;
+            if ((v / 38) == 38)
+                button2.Enabled = false;
+            else
+                button2.Enabled = true;
+            createChart(v);
+       }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //v++;
+            if (v == 0)
+                v += 38;
+            else
+                v++;
+
+            createChart(v);
+            if (v == 0)
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;
+            if (float.Parse(label1.Text) == 1.5 && float.Parse(label2.Text) == 10000)
+                button2.Enabled = false;
+            else
+                button2.Enabled = true;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*using (var fs = File.Open(@"C:\Users\Agnieszka\Desktop\BAZA2.bin", FileMode.Open)) 
+            using (var bs = new BinaryReader(fs)) 
+            {
+                for (var i = 0; i < 16 * 32; i++)
+                {
+                    Console.WriteLine(bs.ReadInt16());
+                }
+            }*/
+            if (File.Exists(fileName))
+            {
+                 this.createChart(v);
+            }
+            else
+                MessageBox.Show("Error while reading file\n");
+            button1.Enabled = false;
+        }
+
+
+            // Read the file into <bits>
+            //var fs = new FileStream(@"C:\Users\Agnieszka\Desktop\BAZA2.bin", FileMode.Open);
+            //var len = (int)fs.Length;
+            //var bits = new byte[len];
+            // fs.Read(bits, 0, len);
+            // Dump 16 bytes per line
+            /* for (int ix = 0; ix < len; ix += 16)
+             {
+                 var cnt = Math.Min(16, len - ix);
+                 var line = new byte[cnt];
+                 Array.Copy(bits, ix, line, 0, cnt);
+                 // Write address + hex + ascii
+                 Console.Write("{0:X6}  ", ix);
+                 Console.Write(BitConverter.ToString(line));
+                 Console.Write("  ");
+                 // Convert non-ascii characters to .
+                 for (int jx = 0; jx < cnt; ++jx)
+                     if (line[jx] < 0x20 || line[jx] > 0x7f) line[jx] = (byte)'.';
+                 Console.WriteLine(Encoding.ASCII.GetString(line));
+             }
+             Console.ReadLine();
+              */
+            /*
+            // Data arrays.
+            string[] seriesArray = { "seria1", "seria2","seria3" };
+            int[] pointsArray = { 1, 2, 3 };
+           
+            // Set palette.
+            this.chart1.Palette = ChartColorPalette.Berry;
+
+            // Set title.
+            this.chart1.Titles.Add("Rt/Rm");
+           
+            // Add series.
+            for (int i = 0; i < seriesArray.Length; i++)
+            {
+                // Add series.
+                Series series = this.chart1.Series.Add(seriesArray[i]);
+                
+                // Add point.
+                
+                series.Points.Add(pointsArray[i]);
+                series.Points.Add(pointsArray[i]);
+                
+                
+            }
+            */
+           /* var series = new Series("Chart");
+            series.Points.DataBindXY(new[] { 1, 2, 3 }, new[] { 1, 2, 3 });
+            chart1.Series.Add(series);
+            series.ChartType = SeriesChartType.Line;
+
+            var series2 = new Series("Chart2");
+            series2.Points.DataBindXY(new[] { 1, 6, 7, 8 }, new[] { 4, 5, 6, 7 });
+            chart1.Series.Add(series2);
+            series2.ChartType = SeriesChartType.Line;
+            * */
+        //}
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            chart1.Width = this.Width-100;
+            chart1.Height = this.Height - 150;
+            //label7.Location = new System.Drawing.Point(chart1.Width/2, chart1.Height+45);
+            //label6.Location = new System.Drawing.Point(20, (chart1.Height +50)/2);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MemoryStream m = new MemoryStream();
+            chart1.SaveImage(m, System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Bmp);
+            Bitmap b = new Bitmap(m);
+            Clipboard.SetImage(b);
+        }
+
+        private void contextMenuStrip1_Opening_1(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void copyToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            MemoryStream m = new MemoryStream();
+            chart1.SaveImage(m, System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Bmp);
+            Bitmap b = new Bitmap(m);
+            Clipboard.SetImage(b);
+        }
+
+         
+
+    }
+
+ }
